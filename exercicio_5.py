e5f597cfb8852e984e5aba708170c56ee56c8b9e
LeonardoModena-cpu <leomodena@hotmail.com>
2026-04-11 17:17:56 -0300

    Update exercicio_5.py

--- exercicio_5.py
+++ exercicio_5.py
@@ -60,10 +60,10 @@ cromossomo = str(input("Digite o nome do cromossomo (ex: chr5): "))
 Posicao = int(input("Digite a posição da variante: "))
 genoma = str(input("Digite o genoma de referência (ex: Hg38): "))
 
-if cromossomo == "chr17" and genoma == "Hg19" and 41196312 < Posicao < 41277500: 
+if cromossomo == "chr17" and genoma == "hg19" and 41196312 < Posicao < 41277500: 
     print("Sim, a variante está no gene BRCA1")
 
-elif cromossomo == "chr17" and genoma == "Hg38" and 43044295 < Posicao < 43125483:
+elif cromossomo == "chr17" and genoma == "hg38" and 43044295 < Posicao < 43125483:
     print("Sim, a variante está no gene BRCA1")
 
 else:
